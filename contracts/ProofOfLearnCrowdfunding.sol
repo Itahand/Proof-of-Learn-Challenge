ba2bb23683add49e4681b238f651f9c9667fb624
Itahand Naizir <itahand.naizir@gmail.com>
2023-03-01 15:52:12 -0600

    Adding events

--- contracts/ProofOfLearnCrowdfunding.sol
+++ contracts/ProofOfLearnCrowdfunding.sol
@@ -16,6 +16,14 @@ interface IERC20 {
  */
 
 contract ProofOfLearnCrowdFunding {
+    ///// Contract variables /////
+    IERC20 public immutable token;
+    uint256 public count;
+    uint256 public maxDuration;
+    mapping(uint256 => Campaign) public campaigns;
+    mapping(uint256 => mapping(address => uint256)) public pledgedAmount;
+
+    ///// Campaign struct with details /////
     struct Campaign {
         address proposer;
         uint256 goal;
@@ -25,12 +33,21 @@ contract ProofOfLearnCrowdFunding {
         bool claimed;
     }
 
-    IERC20 public immutable token;
-    uint256 public count;
-    uint256 public maxDuration;
-    mapping(uint256 => Campaign) public campaigns;
-    mapping(uint256 => mapping(address => uint256)) public pledgedAmount;
+    ///// Events /////
+    event Cancel(uint256 id);
+    event Pledge(uint256 indexed id, address indexed caller, uint256 amount);
+    event Unpledge(uint256 indexed id, address indexed caller, uint256 amount);
+    event Claim(uint256 id);
+    event Refund(uint256 id, address indexed caller, uint256 amount);
+    event Launch(
+        uint256 id,
+        address indexed creator,
+        uint256 goal,
+        uint32 startAt,
+        uint32 endAt
+    );
 
+    ///// Constructor /////
     constructor(address _token, uint256 _maxDuration) {
         token = IERC20(_token);
         maxDuration = _maxDuration;
